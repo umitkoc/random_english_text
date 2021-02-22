e4d79a0d909a3ae8cf3cd2c049227a7678d01601
umitkoc <umitkoc.com@gmail.com>
2021-02-22 21:16:50 +0300

    add word

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ from bs4 import BeautifulSoup as bs
 from requests import get
 from flask import Flask
 
+app=Flask(__name__)
+app.config['Debug']=False
 
-def Text():
+@app.route('/paragraph')
+def Paragraph():
     url="https://randomword.com/paragraph"
     req=get(url)
     html=req.content
     soup=bs(html,'html.parser')
     return soup.find("div",{"id":"random_word_definition"}).text
+@app.route('/word')
+def Word():
+    url="https://randomword.com/"
+    req=get(url)
+    html=req.content
+    soup=bs(html,'html.parser')
+    return soup.find("div",{"id":"random_word"}).text
 
-app=Flask(__name__)
-app.config['Debug']=False
-@app.route('/api',methods=['Get'])
-def Home():
-    return Text()
 
 if __name__=="__main__":
     app.run()
